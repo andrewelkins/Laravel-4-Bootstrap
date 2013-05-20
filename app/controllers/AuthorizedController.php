1668b6ee3dcf1186bfda18f170a200fb77e11a36
Andrew Elkins <github@andrewelkins.com>
2013-05-19 19:11:39 -0700

    Fixes #9

--- app/controllers/AuthorizedController.php
+++ app/controllers/AuthorizedController.php
@@ -4,16 +4,16 @@ class AuthorizedController extends BaseController
 {
 	protected $whitelist = array();
 
-	/**
-	 * Initializer.
-	 *
-	 * @access   public
-	 * @return   void
-	 */
+    /**
+     * Initializer.
+     *
+     * @access   public
+     * @return \AuthorizedController
+     */
 	public function __construct()
 	{
+        parent::__construct();
 		// Check if the user is logged in.
-		//
 		$this->beforeFilter('auth', array('except' => $this->whitelist));
 	}
 }
